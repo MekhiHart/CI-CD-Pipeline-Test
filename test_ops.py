eb6d55827d187ca1ad4c4cc2aee3704efe038cbd
Mekhi Hart Dela Cruz <mekhihartdelacruz@gmail.com>
2023-09-30 16:13:47 -0700

    made basic python calculator funcs

---
+++ test_ops.py
@@ -0,0 +1,13 @@
+from ops import *
+
+def test_add():
+    assert add(2,3) == 5
+
+def test_sub():
+    assert sub(5,2) == 3
+
+def test_mul():
+    assert mul(3,3) == 9
+
+def test_sub():
+    assert div(3,3) == 1
